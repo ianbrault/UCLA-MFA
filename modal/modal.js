738ca6ee2d667e9dd4f2fd9df6f14b265eeccb65
Ian Brault <ian.brault@engineering.ucla.edu>
2018-05-20 18:35:02 -0700

    Add loading spinner.

--- modal/modal.js
+++ modal/modal.js
@@ -4,6 +4,9 @@
 
 document.addEventListener("click", (e) => {
     function getPasscode() {
+        // hide button & display spinner
+        document.getElementById("button").className = "hidden"
+        document.getElementById("spinner").className = "spinner"
         // check URL of active tab
         browser.tabs.query({ 
             currentWindow: true, 
@@ -15,7 +18,12 @@ document.addEventListener("click", (e) => {
                 mode: "cors",
                 headers: { "Tab-Url": tabUrl },
             }).then(res => res.text()).then(code => {
-                console.log(code)
+                // hide spinner, display MFA passcode
+                document.getElementById("spinner").className = "hidden"
+                codeNode = document.createElement("p")
+                codeNode.className = "passcode"
+                codeNode.innerHTML = code
+                document.getElementById("content").appendChild(codeNode)
             })
         }, err => console.error(err))
     }
